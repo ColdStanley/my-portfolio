c9c52ee8e443fd715ad8183393fcbbba9cd35b91
ColdStanley <90196116+ColdStanley@users.noreply.github.com>
2025-05-31 16:34:47 -0400

    fix: disable eslint and ts build errors for Vercel

--- next.config.js
+++ next.config.js
@@ -4,11 +4,9 @@ const nextConfig = {
     domains: ['prod-files-secure.s3.us-west-2.amazonaws.com'],
   },
   eslint: {
-    // ⛔️ 忽略 ESLint 错误，允许构建继续（Vercel 会读取这个配置）
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // ⛔️ 忽略 TS 错误，也一并保险处理
     ignoreBuildErrors: true,
   },
 };
